ef7e41000bbf7f6fae1e0fea840bc2b57fd4e381
Rishabh Parekh <rishabh.parekh00@gmail.com>
2016-10-19 00:07:32 -0400

    Changes to Slot Machine

--- org/wbsd/spin/SlotMachine.java
+++ org/wbsd/spin/SlotMachine.java
@@ -15,21 +15,40 @@ public class SlotMachine {
      Player player = new Player();
      player.setName(name);
 
-     System.out.println("Enter amount to bet ");
+     System.out.println("Enter amount to start with:");
      double amount = scanner.nextDouble();
+     scanner.nextLine(); //throw away the \n not consumed by nextLine()
+
      player.setAmount(amount);
 
      Spinner s = new Spinner();
 
      while (true)
      {
+        if (player.getAmount() <= 0) {
+          System.out.println("Sorry, you are out of money. Goodbye," + player.getName());
+          return;
+        }
+
         System.out.println("Would you like to bet or quit. Enter s to spin and q to quit");
         String option = scanner.nextLine();
+        System.out.println("Option selected is: " + option);
         if (option.equals("q")) {
             System.out.println("You cash-out amount is " + player.getAmount());
             System.out.println("Good bye! " + player.getName());
             return;
         }
+
+        System.out.println("You have " + player.getAmount() + "money left. How much money would you like to bet");
+        double bet = scanner.nextDouble();
+        scanner.nextLine(); //throw away the \n not consumed by nextInt()
+
+        if (bet > player.getAmount()) {
+          System.out.println("Sorry, you are out of money. Goodbye," + player.getName());
+          return;
+        }
+        player.setAmount(player.getAmount() - bet);
+
         List<Integer> numberList = s.spin() ;
         for (ListIterator<Integer> iter = numberList.listIterator(); iter.hasNext(); ) {
             Integer n = iter.next();
@@ -38,7 +57,7 @@ public class SlotMachine {
          System.out.println("");
 
         if (s.fiveInRow()) {
-          player.setAmount(player.getAmount() + 20);
+          player.setAmount(player.getAmount() + 30);
           System.out.println("Five in a row!!! , your new amount is : " + player.getAmount());
         }
         else if (s.fourInRow()) {
@@ -46,17 +65,21 @@ public class SlotMachine {
           System.out.println("Four in a row!!! , your new amount is : " + player.getAmount());
 
         }
-        else if (s.threeInRow()) {
-          player.setAmount(player.getAmount() + 10);
-          System.out.println("Three in a row!!! , your new amount is : " + player.getAmount());
-          if (s.twoInRow()) {
-            player.setAmount(player.getAmount() + 5);
-            System.out.println("And Two in a row!!! , your new amount is : " + player.getAmount());
-          }
+        else if (s.twothreeInRow()) {
+          player.setAmount(player.getAmount() + 8);
+          System.out.println("Three in a row and  two in a row , your new amount is : " + player.getAmount());
         }
-        else if (s.twoInRow()) {
-          player.setAmount(player.getAmount() + 5);
-          System.out.println("Two in a row!!! , your new amount is : " + player.getAmount());
+        else if (s.threeInRow()) {
+          player.setAmount(player.getAmount() + 6);
+          System.out.println("Three in a row!!! , your new amount is : " + player.getAmount());
+        }
+        else {
+          for (int i = 0; i < 4 ; i++ ) {
+            if (s.twoInRow(i)) {
+               player.setAmount(player.getAmount() + 2);
+               System.out.println("Two in a row!!! , your new amount is : " + player.getAmount());
+            }
+          }
         }
       }
    }
--- org/wbsd/spin/Spinner.java
+++ org/wbsd/spin/Spinner.java
@@ -20,64 +20,75 @@ public class Spinner {
   }
 
   public boolean fiveInRow() {
-    int match = 1;
-    for (int i = 0;i < this.draw.size() - 1; i++ ) {
-      int current = this.draw.get(i).intValue();
-      int next = this.draw.get(i+1).intValue();
-      if (current == next) {
-        match++;
-      }
-    }
-    if (match == 5)
-      return true;
-    else
-      return false;
+
+      int match = Collections.frequency(this.draw,this.draw.get(0).intValue());
+      if (match == 5)
+        return true;
+      else
+        return false;
   }
 
 
   public boolean fourInRow() {
-    int match = 1;
-    for (int i = 0;i < this.draw.size() - 1; i++ ) {
-      int current = this.draw.get(i).intValue();
-      int next = this.draw.get(i+1).intValue();
-      if (current == next) {
-        match++;
-      }
-    }
-    if (match == 4)
+    if (((this.draw.get(0).intValue() == this.draw.get(1).intValue()) &&
+         (this.draw.get(1).intValue() == this.draw.get(2).intValue()) &&
+          (this.draw.get(2).intValue() == this.draw.get(3).intValue())
+        ) ||
+        ((this.draw.get(1).intValue() == this.draw.get(2).intValue()) &&
+             (this.draw.get(2).intValue() == this.draw.get(3).intValue()) &&
+              (this.draw.get(3).intValue() == this.draw.get(4).intValue())
+        ))
       return true;
-    else
-      return false;
+     else
+       return false;
+
   }
 
   public boolean threeInRow() {
-    int match = 1;
-    for (int i = 0;i < this.draw.size() - 1; i++ ) {
-      int current = this.draw.get(i).intValue();
-      int next = this.draw.get(i+1).intValue();
-      if (current == next) {
-        match++;
-      }
-    }
-    if (match == 3)
-      return true;
-    else
-      return false;
+
+    if (((this.draw.get(0).intValue() == this.draw.get(1).intValue()) &&
+         (this.draw.get(1).intValue() == this.draw.get(2).intValue()))
+        ||
+       ((this.draw.get(1).intValue() == this.draw.get(2).intValue()) &&
+        (this.draw.get(2).intValue() == this.draw.get(3).intValue()))
+        ||
+       ((this.draw.get(2).intValue() == this.draw.get(3).intValue()) &&
+          (this.draw.get(3).intValue() == this.draw.get(4).intValue()))
+       )
+       return true;
+      else
+        return false;
+
   }
 
-  public boolean twoInRow() {
-    int match = 1;
-    for (int i = 0;i < this.draw.size() - 1; i++ ) {
-      int current = this.draw.get(i).intValue();
-      int next = this.draw.get(i+1).intValue();
-      if (current == next) {
-        match++;
+  public boolean twothreeInRow() {
+
+    if ((this.draw.get(0).intValue() == this.draw.get(1).intValue()) &&
+         (this.draw.get(1).intValue() == this.draw.get(2).intValue())
+        )
+    {
+      if (twoInRow(3))
+      {
+          return true;
       }
     }
-    if (match == 2)
-      return true;
-    else
-      return false;
+
+    if ((this.draw.get(2).intValue() == this.draw.get(3).intValue()) &&
+             (this.draw.get(3).intValue() == this.draw.get(4).intValue())
+        )
+    {
+      if (twoInRow(0))
+      {
+          return true;
+      }
+    }
+    return false;
   }
 
+  public boolean twoInRow(int index) {
+    if (this.draw.get(index).intValue() == this.draw.get(index+1).intValue())
+      return true;
+     else
+      return false;
+  }
 }
