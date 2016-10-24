d9aa76cb4cc254291d1cd09cc2ea0dda0f173d9e
rjain-pivotal <rishabh.parekh00@gmail.com>
2016-10-23 21:13:38 -0400

    Quiz 5

---
+++ q5.java
@@ -0,0 +1,67 @@
+/*
+1-Test Class
+*/
+
+public class q5 {
+   public static void main(String[] args) {
+      //q1  for (int i = 1; i < 100; i = i * 2) {
+      //   if (i / 50 == 0) {
+      //     System.out.println("The Value of I :" + i);
+      //   }
+      // }
+
+      //q2  for (int x = 1 ; x < 5 ; x++) { // Outer Loop is number of rows
+      //   for (int z = 0; z <= x; z ++) { // Inner loop is number of columns
+      //     System.out.print(x*z + " ");
+      //   }
+      //   System.out.println("");
+      // }
+
+      //q3 boolean a, b, c;
+      // a = true;
+      // b = true;
+      // c = (a && b) || !(a || b);
+      //
+      //
+      // System.out.println("Value of a: "  + a);
+      // System.out.println("Value of b: "  + b);
+      //
+      // System.out.println("Value of (a && b): "  + (a && b));
+      // System.out.println("Value of (a || b): " + (a || b));
+      // System.out.println("Value of !(a || b): " + !(a || b));
+      //
+      // System.out.println("Value of c : " + c);
+
+
+      //q4 int x = 10 ;
+      // int y = 3 ;
+      //
+      // boolean b = true;
+      //
+      // for (int i = 0; i < 15; i +=5) {
+      //      x = x + y;
+      //      System.out.println("Value of X = " + x);
+      //      b = (x % y == 2);
+      //      System.out.println("Value of b = " + b);
+      //
+      //      if (!b) {
+      //          y++;
+      //          i +=5;
+      //          System.out.println("Value of y and i = " + y + "  :: " + i);
+      //      }
+      // }
+
+      int i = 1;
+      int k = 1;
+
+      while (k < 5) {
+           k *= i;
+           k++;
+           System.out.println("Value of i and k :" + i + " " + k);
+      }
+
+      System.out.println(k);
+      
+
+   }
+}
